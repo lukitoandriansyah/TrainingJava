79a0c26ac4c8c7f7e672f25a9f8b5de868d8fda7
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-11-01 20:49:26 +0700

    Class And Object di Java

---
+++ src/SelfLearning/ClassAndObject/ClassJava/ClassExample.java
@@ -0,0 +1,12 @@
+package SelfLearning.ClassAndObject.ClassJava;
+
+public class ClassExample {
+    String myName; //Data Member
+    int myAge; //Data Member
+
+    public static void main(String[] args) {
+        ClassExample classExample = new ClassExample(); //Inisialisasi Objek
+        System.out.println(classExample.myAge);
+        System.out.println(classExample.myName);
+    }
+}
